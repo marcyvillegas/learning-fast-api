d97ccc0d6eaa319c0c063cf6cbd3db0abe3427a5
Marcy Villegas <marcyvillegas17@gmail.com>
2023-06-14 18:55:22 +0800

    Added notes regrading FastAPI tutorial

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from models import Books
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, Query, Path, Body
+from models import Books, Item, User
 
 app = FastAPI()
 
@@ -20,3 +21,66 @@ async def getBook(book_name: Books):
     if book_name.value == "book3":
         return {"book": Books.book3}
 
+@app.get("/items/")
+async def getItems(
+    q: Annotated[
+        str | None,
+        Query(
+            title="Query string",
+            description="Query string for the items to search in the database that have a good match",
+            min_length=3,
+        ),
+    ] = None
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}, {"item_id": "Sample1"}, {"item_id": "SAmple2"}, {"item_id": "SAmple3"}]}
+    if q:
+        results.update({"query": q})
+    return results
+
+"""
+Result:
+{
+  "items": [
+    {
+      "item_id": "Foo"
+    },
+    {
+      "item_id": "Bar"
+    },
+    {
+      "item_id": "Sample1"
+    },
+    {
+      "item_id": "SAmple2"
+    },
+    {
+      "item_id": "SAmple3"
+    }
+  ],
+  "q": "dasf"
+}
+"""
+
+@app.get("/search-item/")
+async def searchItems(q: Annotated[str | None, Query(alias="search")] = None):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"searchKeyword": q})
+    return results
+
+@app.get("/items/{item_id}")
+async def read_items(
+    item_id: Annotated[int, Path(title="The ID of the item to get", gt=0, le=1000)],
+    q: Annotated[str | None, Query(alias="item-query")] = None,
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
+
+@app.put("/items/{item_id}")
+async def update_item(
+    item_id: int, item: Item, user: User, importance: Annotated[int, Body()]
+):
+    results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
+    return results
--- models.py
+++ models.py
@@ -1,8 +1,22 @@
 from enum import Enum
+from pydantic import BaseModel
 
-
+# -- PREDEFINED VALUES IN MODEL --
 class Books(str, Enum):
     book1 = "book1"
     book2 = "book2"
     book3 = "book3"
 
+
+# -- REQUEST BODY --
+# data sent from client
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+class User(BaseModel):
+    username: str
+    full_name: str | None = None
