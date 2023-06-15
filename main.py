421699cedeeb16f2869024b15d859b6fee2cec03
Marcy Villegas <marcyvillegas17@gmail.com>
2023-06-15 11:38:53 +0800

    Added Response model - return type sampple codes

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from typing import Annotated
-from fastapi import FastAPI, Query, Path, Body
+from typing import Annotated, Union
+from fastapi import FastAPI, Query, Path, Body, Response
+from pydantic import Required
+from fastapi.responses import JSONResponse, RedirectResponse
+
 from models import Books, Item, User
 
 app = FastAPI()
@@ -21,21 +24,24 @@ async def getBook(book_name: Books):
     if book_name.value == "book3":
         return {"book": Books.book3}
 
+
 @app.get("/items/")
 async def getItems(
-    q: Annotated[
-        str | None,
-        Query(
-            title="Query string",
-            description="Query string for the items to search in the database that have a good match",
-            min_length=3,
-        ),
-    ] = None
+        q: Annotated[
+            str | None,
+            Query(
+                title="Query string",
+                description="Query string for the items to search in the database that have a good match",
+                min_length=3,
+            ),
+        ] = None
 ):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}, {"item_id": "Sample1"}, {"item_id": "SAmple2"}, {"item_id": "SAmple3"}]}
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}, {"item_id": "Sample1"}, {"item_id": "SAmple2"},
+                         {"item_id": "SAmple3"}]}
     if q:
         results.update({"query": q})
     return results
+
 
 """
 Result:
@@ -61,6 +67,7 @@ Result:
 }
 """
 
+
 @app.get("/search-item/")
 async def searchItems(q: Annotated[str | None, Query(alias="search")] = None):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
@@ -68,19 +75,30 @@ async def searchItems(q: Annotated[str | None, Query(alias="search")] = None):
         results.update({"searchKeyword": q})
     return results
 
+
 @app.get("/items/{item_id}")
 async def read_items(
-    item_id: Annotated[int, Path(title="The ID of the item to get", gt=0, le=1000)],
-    q: Annotated[str | None, Query(alias="item-query")] = None,
-):
+        item_id: Annotated[int, Path(title="The ID of the item to get", gt=0, le=1000)],
+        q: Annotated[str | None, Query(alias="item-query")] = None,
+) -> Item:
     results = {"item_id": item_id}
     if q:
         results.update({"q": q})
     return results
 
-@app.put("/items/{item_id}")
+
+@app.put("/items/{item_id}", response_model=dict[str, Union[int, Item, User]])
 async def update_item(
-    item_id: int, item: Item, user: User, importance: Annotated[int, Body()]
+        item_id: int, item: Item, user: User, importance: Annotated[int, Body(gt=10)] = Required
 ):
     results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
     return results
+
+
+@app.get("/rick-roll")
+async def getRickRolled(q: Annotated[bool | None, Query()] = False) -> Response:
+    if q:
+        return RedirectResponse(url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    return JSONResponse(content={"message": "Nah."})
+
+
