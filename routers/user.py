c615664733c4a7b856d7e17976d2ce571ced6ae5
Marcy Villegas <marcyvillegas17@gmail.com>
2023-06-17 19:34:17 +0800

    Added login user endpoint

--- routers/user.py
+++ routers/user.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Depends
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter, Depends, Request, Response, HTTPException
+from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 from models.user import User
-from schemas.user import UserCreate, UserBase
+from schemas.user import UserCreate, UserLogIn
 from db import sessionLocal, engine, Base
 
 app = FastAPI()
@@ -14,6 +14,7 @@ router = APIRouter(
     tags=["user"]
 )
 
+
 def get_db():
     db = sessionLocal()
     try:
@@ -22,10 +23,34 @@ def get_db():
         db.close()
 
 
-@router.post("/add", response_model=UserBase)
+def is_user_logged(request: Request):
+    logged_user = request.cookies.get("logged_user")
+    return logged_user is not None
+
+
+# def create_cookie():
+
+
+@router.post("/signup")
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
     db_user = User(**user.dict())
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
-    return user.dict()
+    return {"message": "User is created!"}
+
+@router.post("/login")
+def login_user(user: UserLogIn, db: Session = Depends(get_db)):
+    existing_user = db.query(User).filter(User.email == user.email).first()
+    if existing_user is None:
+        raise HTTPException(status_code=400, detail="Email is not registered.")
+
+    validate_password = db.query(User).filter(existing_user.password == user.password).first()
+    if validate_password is None:
+        raise HTTPException(status_code=400, detail="Incorrect password.")
+
+    # response = Response({"message": "Login successful!"})
+    # response.set_cookie(key="logged_user", value=user.email)
+    return {"message": "Login successful!"}
+
+
